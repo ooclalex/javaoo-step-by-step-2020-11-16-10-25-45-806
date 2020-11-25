6182372daab5f4cfa68725abeeae2788d88c6923
Alex Liu <LIUAL14@OOCL.COM>
2020-11-25 15:10:24 +0800

    [Alex Liu] Practice05

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,22 @@
 package practice05;
 
-public class Teacher {
+public class Teacher extends Person {
+    private int klass;
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        String strClass = klass != 0 ? "Class " + klass : "No Class";
+        return super.introduce() + " " + "I am a Teacher. I teach " + strClass + ".";
+    }
 }
