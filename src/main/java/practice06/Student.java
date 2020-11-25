16cb0eb405e6e6d6ccf1f579645276bb1d649399
Alex Liu <LIUAL14@OOCL.COM>
2020-11-25 15:34:09 +0800

    [Alex Liu] Practice06

--- src/main/java/practice06/Student.java
+++ src/main/java/practice06/Student.java
@@ -1,4 +1,28 @@
 package practice06;
 
-public class Student {
+public class Student extends Person {
+    private int klassNo;
+    private Klass klass;
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klassNo = klass;
+    }
+
+    public Student(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        return super.introduce() + " " + "I am a Student. I am at Class " + klass.getNumber() + ".";
+    }
 }
--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,43 @@
 package practice06;
 
-public class Teacher {
+import practice05.Person;
+
+public class Teacher extends Person {
+    private int klassNo;
+    private Klass klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klassNo = klass;
+    }
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        String strClass = klass != null ? "Class " + klass.getNumber() : "No Class";
+        return super.introduce() + " " + "I am a Teacher. I teach " + strClass + ".";
+    }
+
+    public String introduceWith(Student student) {
+        StringBuilder intro = new StringBuilder(super.introduce());
+        intro.append(" I am a Teacher. ");
+        if (klass.equals(student.getKlass())) {
+            intro.append("I teach ").append(student.getName()).append(".");
+        }
+        else {
+            intro.append("I don't teach " + student.getName() + ".");
+        }
+        return intro.toString();
+    }
 }
